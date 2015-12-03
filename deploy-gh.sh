3d70eb0e94ba32065b4830e2557d4aad5393074a
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-12-02 23:36:15 -0500

    Clean up service-backed example

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -3,8 +3,8 @@ git branch -D gh-pages
 git push origin --delete gh-pages
 git checkout -b gh-pages
 ember build --environment production
-git rm -rf app addon config tests
-git rm -rf Brocfile.js bower.json package.json testem.json
+git rm -rf app config tests
+git rm -rf bower.json package.json testem.json
 git rm -rf .bowerrc .editorconfig .jshintrc .travis.yml
 mv dist/* .
 rm -rf dist
